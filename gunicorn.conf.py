b407831d4c9e5ac57b0bd44e7d192e727a6b353d
Salman Paris Abdul Hathi <salmanparis.infotech@gmail.com>
2026-07-20 12:28:28 +0400

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,7 @@
+worker_class        = "gevent"
+workers             = 2
+timeout             = 600
+keepalive           = 5
+max_requests        = 100
+max_requests_jitter = 20
+bind                = "0.0.0.0:5050"
